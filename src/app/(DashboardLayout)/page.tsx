dc02c564b93bf0b266d35b16971e1bf4962bdb8e
Kim Jong IL <jngkim@amazon.com>
2024-07-10 01:53:37 +0900

    Project Init

--- src/app/(DashboardLayout)/page.tsx
+++ src/app/(DashboardLayout)/page.tsx
@@ -13,7 +13,7 @@ import Blog from "@/app/(DashboardLayout)/components/dashboard/Blog";
 import MonthlyEarnings from "@/app/(DashboardLayout)/components/dashboard/MonthlyEarnings";
 
 const api = axios.create({
-  baseURL: "https://bw33cfts7a.execute-api.ap-northeast-2.amazonaws.com/prod",
+  baseURL: "https://imd1uugvp4.execute-api.ap-northeast-2.amazonaws.com/prod",
 });
 
 // API 요청 함수
